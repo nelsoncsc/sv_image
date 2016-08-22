4e63163828c0ea2ef0eae328e5c6dae3004d6542
Nelson Campos <nelsoncscampos@gmail.com>
2016-08-21 22:32:33 -0300

    Create RGB2YCbCr.sv
    SystemVerilog implementation of the RGB to YCbCr converter.

---
+++ RGB2YCbCr/RGB2YCbCr.sv
@@ -0,0 +1,15 @@
+module RGB2YCbCr(rgb_if.in in, ycbcr_if.out out);
+
+   always @(posedge in.clk)begin
+       if(in.rst) begin
+          out.Y <= 0;
+          out.Cb <= 0;
+          out.Cr <= 0;
+       end
+       else begin
+          out.Y <= 16+(((in.R<<6)+(in.R<<1)+(in.G<<7)+in.G+(in.B<<4)+(in.B<<3)+in.B)>>8);
+          out.Cb <= 128 + ((-((in.R<<5)+(in.R<<2)+(in.R<<1))-((in.G<<6)+(in.G<<3)+(in.G<<1))+(in.B<<7)-(in.B<<4))>>8);
+          out.Cr <= 128 + (((in.R<<7)-(in.R<<4)-((in.G<<6)+(in.G<<5)-(in.G<<1))-((in.B<<4)+(in.B<<1)))>>8);
+       end
+    end
+endmodule : RGB2YCbCr
